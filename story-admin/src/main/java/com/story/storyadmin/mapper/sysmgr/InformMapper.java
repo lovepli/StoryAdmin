4e38c2eeb2574d8a9971f5dade06be7e86a11e9d
DESKTOP-ELUSA9D\59688 <1171205514@qq.com>
2020-08-17 14:04:28 +0800

    删除项目

--- story-admin/src/main/java/com/story/storyadmin/mapper/sysmgr/InformMapper.java
+++
@@ -1,7 +0,0 @@
-package com.story.storyadmin.mapper.sysmgr;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.story.storyadmin.domain.entity.sysmgr.Inform;
-
-public interface InformMapper extends BaseMapper<Inform> {
-}
