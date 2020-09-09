ce402ac31e02d60ce9239dcf0cfbebc8564f5dd0
DESKTOP-ELUSA9D\59688 <1171205514@qq.com>
2020-09-09 17:38:11 +0800

    对邮件功能做一个说明

--- story-admin/src/main/resources/mail/test.ftl
+++ story-admin/src/main/resources/mail/test.ftl
@@ -6,7 +6,6 @@
 </head>
 <body>
 <h2 align="center" >
-     自定义的邮件模板
     您好${username}, 欢迎来到sundayfine.com! <br/>
     <a href="https://www.sundayfine.com/?id=${id}">点击进入</a>
 
--- story-admin/src/test/java/com/story/storyadmin/service/common/MailServiceTest.java
+++ story-admin/src/test/java/com/story/storyadmin/service/common/MailServiceTest.java
@@ -30,9 +30,10 @@ public class MailServiceTest {
      */
     @Test
     public void sendSimpleMail() {
-        String title = "This is a simple mail.";
-        String content = "This is the content of the mail.";
+        String title = "这是一封简单的邮件.";
+        String content = "这是这封邮件的内容.";
         mailService.sendSimpleMail(sendTo, title, content);
+        System.out.println("000");
     }
 
     /**
@@ -40,12 +41,12 @@ public class MailServiceTest {
      */
     @Test
     public void sendAttachmentsMail() {
-        String title = "This is an email with attachments.";
-        String content ="This is the content of the mail.";
+        String title = "这是一封带带附件的邮件.";
+        String content ="这是这封邮件的内容.";
 
         List<String> attachments = new ArrayList<String>();
-        attachments.add("E:\\web_pic\\006tNc79ly1fj8sf9847xj31kw11zhdw.jpg");
-        attachments.add("E:\\web_pic\\laptop-2590647_1280.jpg");
+        attachments.add("C:\\Users\\59688\\Desktop\\tp\\tp1.jpg");
+        attachments.add("C:\\Users\\59688\\Desktop\\tp\\tp2.jpg");
 
         mailService.sendAttachmentsMail(sendTo,title,content, attachments);
     }
@@ -56,15 +57,15 @@ public class MailServiceTest {
     @Test
     public void sendInlineMail() {
 
-        String title = "This is an email with some static resources.";
+        String title = "这是一封带静态资源的邮件.";
         StringBuilder content = new StringBuilder();
         HashMap<String, String> resMap = new HashMap<>();
 
         String rsc1Id = "resource1";
         String rsc2Id = "resource2";
 
-        resMap.put(rsc1Id, "E:\\web_pic\\006tNc79ly1fj8sf9847xj31kw11zhdw.jpg");
-        resMap.put(rsc2Id, "E:\\web_pic\\laptop-2590647_1280.jpg");
+        resMap.put(rsc1Id, "C:\\Users\\59688\\Desktop\\tp\\tp2.jpg");
+        resMap.put(rsc2Id, "C:\\Users\\59688\\Desktop\\tp\\tp2.jpg");
 
         content.append("<html><head><title>Title</title></head>");
         content.append("<body>");
@@ -79,10 +80,10 @@ public class MailServiceTest {
      */
     @Test
     public void sendTemplateMail() {
-        String title = "This is a template mail with attachments.";
+        String title = "这是一封带附件的模板的邮件.";
         Map<String,Object> context = new HashMap<>();
         context.put("id", "2091");
-        context.put("username", "nj.sun");
+        context.put("username", "lovepli");
 
         List<String> attachments = new ArrayList<>();
         attachments.add("C:\\Users\\sunnj\\Desktop\\test.txt");
