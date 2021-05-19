22c57fa839ea3278ae003d5c73f2e9fae81eaded
lovepli <lovepli.cn@gmail.com>
2021-05-19 17:34:19 +0800

    后端拿到前端传过来的时间范围对象的处理
    后端拿到前端传过来的时间范围对象的处理

--- story-admin/src/main/java/com/story/storyadmin/utils/PageUtil.java
+++ story-admin/src/main/java/com/story/storyadmin/utils/PageUtil.java
@@ -3,10 +3,12 @@ package com.story.storyadmin.utils;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 
+import java.security.spec.KeySpec;
+
 /**
  * @author: 59688
  * @date: 2021/4/26
- * @description: 分页工具类
+ * @description: 分页工具类和日期格式化
  */
 public class PageUtil<T> {
 
@@ -36,5 +38,21 @@ public class PageUtil<T> {
         page.setTotal(total);
         return page;
     }
+
+    /**
+     * 从前端传递给后端的json对象中取出时间范围数组，然后将时间范围转换为两个时间设置到json中
+     * @param jsonObject
+     */
+    public static void makeDateRangeToJson(JSONObject jsonObject){
+        String startTime ="";
+        String endTime = "";
+        if(jsonObject.getJSONArray("dateRange")!=null && jsonObject.getJSONArray("dateRange").size()>0){
+            startTime = jsonObject.getJSONArray("dateRange").getString(0);
+            endTime = jsonObject.getJSONArray("dateRange").getString(1);
+        }
+        jsonObject.put("startTime",startTime);
+        jsonObject.put("endTime",endTime);
+    }
+
 
 }
