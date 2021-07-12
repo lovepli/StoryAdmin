cf900d4cf206ee3b234a85b8a149bb84573d305f
lovepli <lovepli.cn@gmail.com>
2021-07-12 17:03:35 +0800

    定时任务+线程池实现定时任务
    定时任务+线程池实现定时任务

--- story-admin/src/main/java/com/story/storyadmin/StoryAdminApplication.java
+++ story-admin/src/main/java/com/story/storyadmin/StoryAdminApplication.java
@@ -5,11 +5,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.story.storyadmin.mapper")
 @EnableAsync // springboot 项目启动类上加@EnableAsync注解即可以实现方法的异步执行，无需再继承Thread类或实现Runable接口，在需要异步执行的方法上添加@Async注解，该方法即可以异步执行
 @EnableCrosXssFilter// 增加自定义注解开关，需要使用的时候只需在springboot启动类加上@EnableCrosXssFilter注解即可开启 CrosXssFilter 进行防xss的过滤器的使用。
+//@EnableScheduling // 在启动类上添加注解@EnableScheduling 开启定时任务
 public class StoryAdminApplication {
 
     /**
